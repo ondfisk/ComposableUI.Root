ecdc8821f16930881137aa942cf0710673ef95c3
Rasmus Lystrøm <rasmusl@microsoft.com>
2023-09-20 21:38:45 +0200

    Split server and client

---
+++ ComposableUI.Client/DownstreamApiAuthorizationMessageHandler.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+namespace ComposableUI.Client;
+
+public class DownstreamApiAuthorizationMessageHandler : AuthorizationMessageHandler
+{
+    public DownstreamApiAuthorizationMessageHandler(IAccessTokenProvider provider,
+        NavigationManager navigation, IConfiguration config)
+        : base(provider, navigation)
+    {
+        ConfigureHandler(
+            authorizedUrls: new[] { config.GetSection("DownstreamApi")["BaseUrl"] },
+            scopes: config.GetSection("DownstreamApi:Scopes").Get<List<string>>());
+    }
+}
---
+++ ComposableUI.Client/Program.cs
@@ -0,0 +1,25 @@
+using ComposableUI.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<DownstreamApiAuthorizationMessageHandler>();
+builder.Services.AddHttpClient("ComposableUI.DownstreamApi", client => client.BaseAddress = new Uri(builder.Configuration.GetSection("DownstreamApi")["BaseUrl"]))
+    .AddHttpMessageHandler<DownstreamApiAuthorizationMessageHandler>();
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("ComposableUI.DownstreamApi"));
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    foreach (var scope in builder.Configuration.GetSection("DownstreamApi:Scopes").Get<List<string>>())
+    {
+        options.ProviderOptions.DefaultAccessTokenScopes.Add(scope);
+    }   
+});
+
+await builder.Build().RunAsync();
